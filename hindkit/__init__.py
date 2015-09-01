7f41b34bee37baae892caa791cccf89892c394e6
Liang Hai <lianghai@gmail.com>
2015-09-01 21:22:17 +0800

    0.2.1

--- hindkit/__init__.py
+++ hindkit/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import division, print_function, unicode_literals
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 from hindkit.constants import paths, linguistics, styles, templates
 from hindkit.family    import Family, Master, Style
