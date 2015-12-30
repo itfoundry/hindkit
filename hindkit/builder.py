94140e9a5ca1cd341d82b68a48e0386d44239864
Liang Hai <lianghai@gmail.com>
2015-12-30 22:36:40 +0530

    _unwrap_the_path_relative_to_working_dir()

--- hindkit/builder.py
+++ hindkit/builder.py
@@ -92,8 +92,11 @@ class Builder(object):
         subprocess.call(['rm', '-fr', hindkit.constants.paths.BUILD])
         subprocess.call(['mkdir', hindkit.constants.paths.BUILD])
 
-    def normalize_path(self, path):
-        return os.path.join(self.family.working_directory, path)
+    def _unwrap_the_path_relative_to_package_dir(self, relative_path):
+        return os.path.join(hindkit.__path__[0], relative_path)
+
+    def _unwrap_the_path_relative_to_working_dir(self, relative_path):
+        return os.path.join(self.family.working_directory, relative_path)
 
     def set_options(self, options = []):
 
@@ -123,14 +126,14 @@ class Builder(object):
     def generate_designspace(self):
 
         doc = mutatorMath.ufo.document.DesignSpaceDocumentWriter(
-            self.normalize_path(hindkit.constants.paths.DESIGNSPACE)
+            self._unwrap_the_path_relative_to_working_dir(hindkit.constants.paths.DESIGNSPACE)
         )
 
         for i, master in enumerate(self.family.masters):
 
             doc.addSource(
 
-                path = self.normalize_path(master.path),
+                path = self._unwrap_the_path_relative_to_working_dir(master.path),
                 name = 'master-' + master.name,
                 location = {'weight': master.interpolation_value},
 
@@ -151,7 +154,7 @@ class Builder(object):
                 location = {'weight': style.interpolation_value},
                 familyName = self.family.output_name,
                 styleName = style.name,
-                fileName = self.normalize_path(style.path),
+                fileName = self._unwrap_the_path_relative_to_working_dir(style.path),
                 postScriptFontName = style.output_full_name_postscript,
                 # styleMapFamilyName = None,
                 # styleMapStyleName = None,
@@ -224,10 +227,10 @@ class Builder(object):
                     'working_directory': self.family.working_directory,
                 }
 
-                font_source_path = self.normalize_path(info['source_path'])
+                font_source_path = self._unwrap_the_path_relative_to_working_dir(info['source_path'])
                 font_source = defcon.Font(font_source_path)
 
-                font_target_path = self.normalize_path(info['target_path'])
+                font_target_path = self._unwrap_the_path_relative_to_working_dir(info['target_path'])
                 font_target = defcon.Font(font_target_path)
 
                 new_names = set(font_source.keys())
