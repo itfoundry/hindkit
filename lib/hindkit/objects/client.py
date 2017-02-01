9599138a2385e40f37954432e351c4445d7ccc73
Liang Hai <lianghai@gmail.com>
2017-02-01 21:02:00 +0800

    Correct Client

--- lib/hindkit/objects/client.py
+++ lib/hindkit/objects/client.py
@@ -36,7 +36,7 @@ class Client(object):
             11: "https://indiantypefoundry.com",
             12: self.family.info.openTypeNameDesignerURL,
             13: "This Font Software is protected under domestic and international trademark and copyright law. You agree to identify the ITF fonts by name and credit the ITF's ownership of the trademarks and copyrights in any design or production credits.",
-            14: "http://indiantypefoundry.com/licensing/eula/",
+            14: "https://indiantypefoundry.com/licensing",
             19: self.family.script.sample_text,
         }
         self.tables["OS/2"] = {
@@ -53,10 +53,9 @@ class Client(object):
             self.tables["name"].update({
                 0: kit.fallback(
                     self.family.info.copyright,
-                    "Copyright (c) {} Indian Type Foundry (info@indiantypefoundry.com)".format(datetime.date.today().year),
+                    "Copyright (c) {} Indian Type Foundry (info@indiantypefoundry.com)".format(self.release_year_range),
                 ),
                 7: None,
-                11: "http://indiantypefoundry.com/googlefonts",
                 13: "This Font Software is licensed under the SIL Open Font License, Version 1.1. This license is available with a FAQ at: http://scripts.sil.org/OFL",
                 14: "http://scripts.sil.org/OFL",
             })
