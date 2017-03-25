f7ec9201e5bd784b9612deb39cd5847254a4f28d
Liang Hai <lianghai@gmail.com>
2017-03-26 01:35:27 +0800

    Change quotation marks

--- lab/generate_GOADB.py
+++ lab/generate_GOADB.py
@@ -17,10 +17,10 @@ class GlyphSet(object):
             else:
                 row = [glyph.name, glyph.name]
             if glyph.unicode_mapping:
-                row.append('uni' + glyph.unicode_mapping)
-            line = ' '.join(row)
+                row.append("uni" + glyph.unicode_mapping)
+            line = " ".join(row)
             goadb_lines.append(line)
-        goadb = '\n'.join(goadb_lines) + '\n'
+        goadb = "\n".join(goadb_lines) + "\n"
         return goadb
 
 u_name_to_u_scalar = {v: k for k, v in k.constants.get_u_scalar_to_u_name().items()}
@@ -30,37 +30,37 @@ class Glyph(object):
     cstems = k.constants.CONSONANT_STEMS
     vstems = k.constants.VOWEL_STEMS
 
-    half_map = {s: (s + 'A_Virama') for s in cstems}
-    nukta_map = {(s + 'xA'): (s + 'A_Nukta') for s in cstems}
-    half_nukta_map = {(s + 'x'): (s + 'A_Nukta_Virama') for s in cstems}
-    c2_map = {(s + 'Ac2'): ('Virama_' + s + 'A') for s in cstems}
+    half_map = {s: (s + "A_Virama") for s in cstems}
+    nukta_map = {(s + "xA"): (s + "A_Nukta") for s in cstems}
+    half_nukta_map = {(s + "x"): (s + "A_Nukta_Virama") for s in cstems}
+    c2_map = {(s + "Ac2"): ("Virama_" + s + "A") for s in cstems}
     cv_map = {}
     for v in vstems:
         for c in cstems:
-            cv_map[c + v] = c + 'A_m' + v
-        cv_map['KSS' + v] = 'KA_Virama_SSA_m' + v
+            cv_map[c + v] = c + "A_m" + v
+        cv_map["KSS" + v] = "KA_Virama_SSA_m" + v
 
     shorthand_flattening_map = {
-        'Reph': 'RA_Virama',
-        'Eyelash': 'RA_Virama_zerowidthjoiner',
-        'Rashtrasign': 'Virama_RA',
-        'Rakar': 'Virama_RA',
-        'RAphalaa': 'Virama_RA',
-        'BAphalaa': 'Virama_BA',
-        'YAphalaa': 'Virama_YA',
+        "Reph": "RA_Virama",
+        "Eyelash": "RA_Virama_zerowidthjoiner",
+        "Rashtrasign": "Virama_RA",
+        "Rakar": "Virama_RA",
+        "RAphalaa": "Virama_RA",
+        "BAphalaa": "Virama_BA",
+        "YAphalaa": "Virama_YA",
     }
     for m in [half_map, nukta_map, half_nukta_map, c2_map, cv_map]:
         shorthand_flattening_map.update(m)
 
     name_to_scalar_map  = {
-        'space': '0020',
-        'rupee': '20A8',
-        'indianrupee': '20B9',
-        'apostrophemod': '02BC',
-        'zerowidthspace': '200B',
-        'zerowidthnonjoiner': '200C',
-        'zerowidthjoiner': '200D',
-        'dottedcircle': '25CC',
+        "space": "0020",
+        "rupee": "20A8",
+        "indianrupee": "20B9",
+        "apostrophemod": "02BC",
+        "zerowidthspace": "200B",
+        "zerowidthnonjoiner": "200C",
+        "zerowidthjoiner": "200D",
+        "dottedcircle": "25CC",
     }
     name_to_scalar_map.update({
         glyph_name: u_name_to_u_scalar[u_name]
@@ -69,10 +69,10 @@ class Glyph(object):
 
     def __init__(
         self,
-        development_name = '',
-        unicode_mapping = '',
-        production_name = '',
-        character_sequence = '', #TODO
+        development_name = "",
+        unicode_mapping = "",
+        production_name = "",
+        character_sequence = "", #TODO
     ):
         if development_name:
             self.name = development_name
@@ -98,14 +98,14 @@ class Glyph(object):
             self.unicode_mapping = self._get_unicode_mapping()
 
     def _get_unicode_mapping(self):
-        return self.name_to_scalar_map.get(self.name, '')
+        return self.name_to_scalar_map.get(self.name, "")
 
     def _get_name_from_unicode_mapping(self):
-        return kit.linguistics.SCALAR_TO_NAME_MAP.get(self.unicode_mapping, 'uni' + unicode_mapping)
+        return kit.linguistics.SCALAR_TO_NAME_MAP.get(self.unicode_mapping, "uni" + unicode_mapping)
 
     def _get_name_structure(self):
         name = self.name
-        prefix = ''
+        prefix = ""
         if name.startswith(
             tuple(
                 i.abbr for i in k.constants.SCRIPT_NAMES_TO_SCRIPTS.values()
@@ -113,11 +113,11 @@ class Glyph(object):
         ):
             prefix = name[:2]
             name = name[2:]
-        stem, sep, suffix = name.partition('.')
-        stem_pieces = stem.split('_')
-        suffixes = suffix.split('.')
-        if name == '.notdef':
-            stem = '.notdef'
+        stem, sep, suffix = name.partition(".")
+        stem_pieces = stem.split("_")
+        suffixes = suffix.split(".")
+        if name == ".notdef":
+            stem = ".notdef"
             stem_pieces = [stem]
             suffixes = []
         return prefix, stem, stem_pieces, suffixes
@@ -130,14 +130,14 @@ class Glyph(object):
 
         splits = []
         for s in splits_with_underscores:
-            splits.extend(s.split('_'))
+            splits.extend(s.split("_"))
 
         scalars = []
         for split in splits:
 
             prefix_for_this_split = self.prefix
-            if split in ['zerowidthjoiner', 'udatta', 'anudatta']:
-                prefix_for_this_split = ''
+            if split in ["zerowidthjoiner", "udatta", "anudatta"]:
+                prefix_for_this_split = ""
             name = prefix_for_this_split + split
 
             if name in k.constants.get_glyph_list("aglfn.txt"):
@@ -145,13 +145,13 @@ class Glyph(object):
             else:
                 scalar = self.name_to_scalar_map.get(name)
                 if scalar:
-                    scalars.append('uni' + scalar)
+                    scalars.append("uni" + scalar)
                 else:
-                    scalars.append('{' + name + '}')
+                    scalars.append("{" + name + "}")
 
         suffixes = self.suffixes[:]
-        if self.stem_pieces == ['Reph']: # Information lost in scalars gets into suffixes
-            suffixes.insert(0, 'reph')
+        if self.stem_pieces == ["Reph"]: # Information lost in scalars gets into suffixes
+            suffixes.insert(0, "reph")
         suffixes = filter(None, suffixes)
 
         return scalars, suffixes
@@ -160,25 +160,25 @@ class Glyph(object):
         scalars, suffixes = self._get_scalars()
         production_name = scalars[0]
         for s in scalars[1:]:
-            if s.startswith('uni'):
+            if s.startswith("uni"):
                 production_name += s[3:]
             else:
                 production_name += s
         if suffixes:
-            production_name += '.' + '.'.join(suffixes)
+            production_name += "." + ".".join(suffixes)
         return production_name
 
     def _get_sequence(self):
         scalars, suffixes = self._get_scalars()
         characters = []
         for s in scalars:
-            if s.startswith('uni'):
-                characters.append(('\\u' + s[3:]).decode('unicode_escape'))
+            if s.startswith("uni"):
+                characters.append(("\\u" + s[3:]).decode("unicode_escape"))
             else:
                 characters.append(s)
-        sequence = ''.join(characters)
+        sequence = "".join(characters)
         if suffixes:
-            sequence += '.' + '.'.join(suffixes)
+            sequence += "." + ".".join(suffixes)
         return sequence
 
     def _get_name_from_production_name(self, production_name):
--- lab/generate_GSUB_features.py
+++ lab/generate_GSUB_features.py
@@ -4,7 +4,7 @@ from __future__ import division, absolute_import, print_function, unicode_litera
 
 import collections, re
 import hindkit as k
-from glyphs import Glyph
+from generate_GOADB import Glyph
 
 DO_HACK_FOR_CORE_TEXT = True
 
@@ -12,19 +12,19 @@ DO_OUTPUT_INDIC1 = True
 DO_OUTPUT_INDIC2 = True
 DO_OUTPUT_USE = True
 
-PREFIX = 'dv'
+PREFIX = "dv"
 
 def indent(line):
-    return '  ' + line
+    return "  " + line
 
 def comment(line):
-    return '# ' + line
+    return "# " + line
 
 def add_prefix(name):
     return PREFIX + name
 
 def remove_prefix(name):
-    if name.startswith('dv'):
+    if name.startswith("dv"):
         name = name[2:]
     return name
 
@@ -61,10 +61,10 @@ class Feature(object):
 
     def dump(self):
         lines = []
-        lines.append('feature {} {{'.format(self.tag))
+        lines.append("feature {} {{".format(self.tag))
         for l in self.lookups.values():
             lines.extend(map(indent, l.dump()))
-        lines.append('}} {};'.format(self.tag))
+        lines.append("}} {};".format(self.tag))
         return lines
 
 class Lookup(object):
@@ -81,9 +81,9 @@ class Lookup(object):
 
     def dump(self):
         lines = []
-        lines.append('lookup {} {{'.format(self.label))
+        lines.append("lookup {} {{".format(self.label))
         lines.extend([indent(r.dump()) for r in self.rules])
-        lines.append('}} {};'.format(self.label))
+        lines.append("}} {};".format(self.label))
         if not self.rules:
             lines = map(comment, lines)
         return lines
@@ -99,9 +99,9 @@ class Rule(object):
     @property
     def text(self):
         if self._text is None:
-            text = 'sub {} by {};'.format(
-                ' '.join(self.input_components),
-                ' '.join(self.output_components),
+            text = "sub {} by {};".format(
+                " ".join(self.input_components),
+                " ".join(self.output_components),
             )
         else:
             text = self._text
@@ -117,21 +117,21 @@ class Rule(object):
     def dump(self):
         dump = self.text
         if self.is_empty:
-            dump = ' '.join(self.output_components)
+            dump = " ".join(self.output_components)
             self.is_commented = True
         if self.is_commented:
-            dump = '# ' + dump
+            dump = "# " + dump
         return dump
 
-PLAIN_CONSONANTS_LETTERS = [i + 'A' for i in k.constants.CONSONANT_STEMS]
-AKHAND_LIGATURES = ['K_SSA', 'J_NYA']
+PLAIN_CONSONANTS_LETTERS = [i + "A" for i in k.constants.CONSONANT_STEMS]
+AKHAND_LIGATURES = ["K_SSA", "J_NYA"]
 
 BASIC_FORMS = PLAIN_CONSONANTS_LETTERS + AKHAND_LIGATURES
 
-NUKTA_FORMS = [i + 'xA' for i in k.constants.CONSONANT_STEMS]
+NUKTA_FORMS = [i + "xA" for i in k.constants.CONSONANT_STEMS]
 
-RAKAR_FORMS = [i[:-1] + '_RA' for i in BASIC_FORMS]
-RAKAR_NUKTA_FORMS = [i[:-1] + '_RA' for i in NUKTA_FORMS]
+RAKAR_FORMS = [i[:-1] + "_RA" for i in BASIC_FORMS]
+RAKAR_NUKTA_FORMS = [i[:-1] + "_RA" for i in NUKTA_FORMS]
 
 HALF_FORMS = [i[:-1] for i in BASIC_FORMS]
 HALF_NUKTA_FORMS = [i[:-1] for i in NUKTA_FORMS]
@@ -140,7 +140,7 @@ HALF_RAKAR_NUKTA_FORMS = [i[:-1] for i in RAKAR_NUKTA_FORMS]
 
 CANONICAL_FORMED_GLYPHS = BASIC_FORMS + NUKTA_FORMS + RAKAR_FORMS + RAKAR_NUKTA_FORMS + HALF_FORMS + HALF_NUKTA_FORMS + HALF_RAKAR_FORMS + HALF_RAKAR_NUKTA_FORMS
 
-def get_components(name, mode='indic2'):
+def get_components(name, mode="indic2"):
     g = Glyph(name)
     global PREFIX
     PREFIX = g.prefix
@@ -149,51 +149,51 @@ def get_components(name, mode='indic2'):
     suffixes = g.suffixes
     components = input_pieces
     lookup = None
-    if PREFIX != 'dv':
+    if PREFIX != "dv":
         pass
-    elif suffixes != ['']:
+    elif suffixes != [""]:
         pass
-    elif stem == 'Reph':
-        lookup = 'rphf'
-        components = ['RA', 'Virama']
-    elif stem == 'RAc2':
-        if mode == 'indic2':
-            lookup = 'blwf_new'
-            components = ['Virama', 'RA']
-        elif mode == 'indic1':
-            lookup = 'blwf_old'
-            components = ['RA', 'Virama']
-    elif stem == 'Eyelash':
-        lookup = 'akhn_eyelash'
-        components = ['RA\'', 'Virama\'', 'zerowidthjoiner']
+    elif stem == "Reph":
+        lookup = "rphf"
+        components = ["RA", "Virama"]
+    elif stem == "RAc2":
+        if mode == "indic2":
+            lookup = "blwf_new"
+            components = ["Virama", "RA"]
+        elif mode == "indic1":
+            lookup = "blwf_old"
+            components = ["RA", "Virama"]
+    elif stem == "Eyelash":
+        lookup = "akhn_eyelash"
+        components = ["RA'", "Virama'", "zerowidthjoiner"]
     elif stem in PLAIN_CONSONANTS_LETTERS:
         pass
     elif stem in AKHAND_LIGATURES:
-        lookup = 'akhn'
-        if stem == 'K_SSA':
-            components = ['KA', 'Virama', 'SSA']
-        elif stem == 'J_NYA':
-            components = ['JA', 'Virama', 'NYA']
+        lookup = "akhn"
+        if stem == "K_SSA":
+            components = ["KA", "Virama", "SSA"]
+        elif stem == "J_NYA":
+            components = ["JA", "Virama", "NYA"]
     elif stem in NUKTA_FORMS:
-        lookup = 'nukt'
-        components = [stem[:-2] + 'A', 'Nukta']
+        lookup = "nukt"
+        components = [stem[:-2] + "A", "Nukta"]
     elif stem in HALF_FORMS + HALF_NUKTA_FORMS:
-        lookup = 'half'
-        components = [stem + 'A', 'Virama']
+        lookup = "half"
+        components = [stem + "A", "Virama"]
     elif stem in RAKAR_FORMS + RAKAR_NUKTA_FORMS:
-        if mode == 'indic2':
-            lookup = 'rkrf_new'
-            components = [stem[:-3] + 'A', 'Virama', 'RA']
-        elif mode == 'indic1':
-            lookup = 'vatu_old'
-            components = [stem[:-3] + 'A', 'RAc2']
+        if mode == "indic2":
+            lookup = "rkrf_new"
+            components = [stem[:-3] + "A", "Virama", "RA"]
+        elif mode == "indic1":
+            lookup = "vatu_old"
+            components = [stem[:-3] + "A", "RAc2"]
     elif stem in HALF_RAKAR_FORMS + HALF_RAKAR_NUKTA_FORMS:
-        if mode == 'indic2':
-            lookup = 'half_new'
-            components = [stem + 'A', 'Virama']
-        elif mode == 'indic1':
-            lookup = 'vatu_old'
-            components = [stem[:-2], 'RAc2']
+        if mode == "indic2":
+            lookup = "half_new"
+            components = [stem + "A", "Virama"]
+        elif mode == "indic1":
+            lookup = "vatu_old"
+            components = [stem[:-2], "RAc2"]
     components = map(add_prefix, components)
     return components, lookup
 
@@ -201,7 +201,7 @@ def ligate(components, reference):
     output = [components[0]]
     for a in components[1:]:
         b = output[-1]
-        b_a = b + '_' + a
+        b_a = b + "_" + a
         if b_a in reference:
             output.pop()
             output.append(b_a)
@@ -217,28 +217,28 @@ def generate_cjct(components, formed_glyphs):
     for a in components:
 
         is_different = False
-        if 'dv' + a not in formed_glyphs:
+        if "dv" + a not in formed_glyphs:
             if a in RAKAR_FORMS + RAKAR_NUKTA_FORMS + HALF_FORMS + HALF_NUKTA_FORMS + HALF_RAKAR_FORMS + HALF_RAKAR_NUKTA_FORMS:
                 if not is_cjct:
                     is_cjct = True
                     output = {
-                        'cjct_new': output[:],
-                        'pres_old': output[:],
+                        "cjct_new": output[:],
+                        "pres_old": output[:],
                     }
                 if a in HALF_FORMS + HALF_NUKTA_FORMS:
-                    extension = [a + 'A', 'Virama']
+                    extension = [a + "A", "Virama"]
                 elif a in RAKAR_FORMS + RAKAR_NUKTA_FORMS:
-                    extension = [a[:-3] + 'A', 'RAc2']
+                    extension = [a[:-3] + "A", "RAc2"]
                 elif a in HALF_RAKAR_FORMS + HALF_RAKAR_NUKTA_FORMS:
                     is_different = True
-                    if 'dv' + a + 'A' in formed_glyphs:
-                        extension_indic2 = [a + 'A', 'Virama']
+                    if "dv" + a + "A" in formed_glyphs:
+                        extension_indic2 = [a + "A", "Virama"]
                     else:
-                        extension_indic2 = [a[:-2] + 'A', 'RAc2', 'Virama']
-                    if 'dv' + a[:-2] in formed_glyphs:
-                        extension_indic1 = [a[:-2], 'RAc2']
+                        extension_indic2 = [a[:-2] + "A", "RAc2", "Virama"]
+                    if "dv" + a[:-2] in formed_glyphs:
+                        extension_indic1 = [a[:-2], "RAc2"]
                     else:
-                        extension_indic1 = [a[:-2] + 'A', 'Virama', 'RAc2']
+                        extension_indic1 = [a[:-2] + "A", "Virama", "RAc2"]
             else:
                 extension = [a]
         else:
@@ -252,8 +252,8 @@ def generate_cjct(components, formed_glyphs):
             extension_pres_old = extension
 
         if is_cjct:
-            output['cjct_new'].extend(extension_cjct_new)
-            output['pres_old'].extend(extension_pres_old)
+            output["cjct_new"].extend(extension_cjct_new)
+            output["pres_old"].extend(extension_pres_old)
         else:
             output.extend(extension)
 
