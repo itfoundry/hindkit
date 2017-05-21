83c55fc60b131205089136739b08fe594d58a1c3
Liang Hai <lianghai@gmail.com>
2017-05-21 00:24:54 -0600

    Correct py_modules

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env AFDKOPython
 import setuptools
 setuptools.setup(
-    name = 'hindkit',
-    version = '1.0.0',
+    name = "hindkit",
+    version = "1.0.0",
     url = "https://github.com/itfoundry/hindkit",
-    author = 'Liang Hai',
-    author_email = 'lianghai@gmail.com',
-    license='MIT',
-    package_dir = {'': 'lib'},
+    author = "Liang Hai",
+    author_email = "lianghai@gmail.com",
+    license="MIT",
+    package_dir = {"": "lib"},
     py_modules = [
-        'WriteFeaturesKernFDK',
-        'WriteFeaturesMarkFDK',
+        "getKerningPairsFromFEA"
+        "WriteFeaturesKernFDK",
+        "WriteFeaturesMarkFDK",
     ],
     packages = [
-        'hindkit',
-        'hindkit.objects',
+        "hindkit",
+        "hindkit.objects",
     ],
     package_data = {
-        'hindkit': [
-            'data/*.txt',
-            'data/premade/*/GlyphOrderAndAliasDB',
-            'data/premade/*/features/*.fea',
+        "hindkit": [
+            "data/*.txt",
+            "data/premade/*/GlyphOrderAndAliasDB",
+            "data/premade/*/features/*.fea",
         ],
     },
 )
